19d91e334d1d379f1f9e8dddbf95c0b5a11782de
dsDomingossobrinho <88497747+DomingosSobrinhoDS@users.noreply.github.com>
2025-03-05 03:10:49 +0100

    project basis

---
+++ src/main/java/ao/multaplus/MultaplusApplication.java
@@ -0,0 +1,13 @@
+package ao.multaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultaplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/multaplus/MultaplusApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.multaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultaplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
