a5690362b1bf3b9e80bc8e49e2bed781c7fefd5b
Marcelino <mfigueira1901@gmail.com>
2025-03-16 03:42:11 +0100

    update auth endpoint

--- src/main/java/ao/multaplus/auth/controller/AuthController.java
+++ src/main/java/ao/multaplus/auth/controller/AuthController.java
@@ -8,15 +8,11 @@ import ao.multaplus.auth.service.AuthService;
 import ao.multaplus.security.SecurityConfigurations;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,7 +23,6 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
-
     @PostMapping("/login")
     @Operation(summary = "Login", description = "Login To get the token")
     public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginDto loginDto) {
