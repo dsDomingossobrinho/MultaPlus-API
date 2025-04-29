d5bd424c092f7379f651128176b9f23746b93489
Marcelino <mfigueira1901@gmail.com>
2025-04-29 20:24:10 +0100

    Add get gender

--- src/main/java/ao/multaplus/gender/service/GenderServiceImpl.java
+++ src/main/java/ao/multaplus/gender/service/GenderServiceImpl.java
@@ -78,4 +78,9 @@ public class GenderServiceImpl implements GenderService {
     public Optional<Genders> findone(long id) {
         return genderRepository.findById(id);
     }
+
+    @Override
+    public Genders getGender(Long id) {
+        return genderRepository.findById(id).orElseThrow(()->{throw new RuntimeException("gender not found");});
+    }
 }
